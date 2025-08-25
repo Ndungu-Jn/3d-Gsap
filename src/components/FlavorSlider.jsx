670b2095c4e3c6c42a0b4370fc9e9d38743ac3f1
John Ndungu <ndungulink@gmail.com>
2025-08-25 10:11:52 +0300

    Add FlavorSlider component with flavor images and styles

--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -1,5 +1,29 @@
+import { flavorlists } from "../constants";
+
 const FlavorSlider = () => {
-  return <div>FlavorSlider</div>;
+  return (
+    <div className="slider-wrapper">
+      <div className="flavors">
+        {flavorlists.map((flavor) => (
+          <div
+            key={flavor.name}
+            className="relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] md:h-[50vh] h-80 flex-none"
+          >
+            <img
+              src={`/images/${flavor.color}-bg.svg`}
+              alt=""
+              className="absolute bottom-0"
+            />
+            <img
+              src={`/images/${flavor.color}-drink.webp`}
+              alt=""
+              className="drinks"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FlavorSlider;
