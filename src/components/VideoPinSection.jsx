60e38d67f27f0843e90ae3259770300d392720be
John Ndungu <ndungulink@gmail.com>
2025-09-03 09:37:12 +0300

    Add VideoPinSection component with video playback and play button

--- src/components/VideoPinSection.jsx
+++ src/components/VideoPinSection.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const VideoPinSection = () => {
-  return <div>VideoPinSection</div>;
+  return (
+    <section className="vd-pin-section">
+      <div style={{}} className="size-full video-box">
+        <video
+          src="/videos/pin-video.mp4"
+          playsInline
+          muted
+          loop
+          autoPlay
+        ></video>
+        <div className="abs-center md:scale-100 scale-200">
+          <img src="/images/circle-text.svg" alt="" className="spin-circle" />
+          <div className="play-btn">
+            <img src="/images/play.svg" alt="" className="size-[3vw]" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default VideoPinSection;
