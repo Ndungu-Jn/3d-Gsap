ad0db6ffd0c0066189a6ade77bfd5a59b182cb8f
John Ndungu <ndungulink@gmail.com>
2025-09-04 15:01:11 +0300

    Refactor VideoPinSection to conditionally apply GSAP animations based on mobile viewport

--- src/components/VideoPinSection.jsx
+++ src/components/VideoPinSection.jsx
@@ -1,29 +1,37 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
 
 const VideoPinSection = () => {
+  const isMobile = useMediaQuery({
+    query: "max-width:768px",
+  });
   useGSAP(() => {
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".vd-pin-section",
-        start: "-15% top",
-        end: "200% top",
-        scrub: 1.5,
-        markers: true,
-        pin: true,
-      },
-    });
+    if (!isMobile) {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".vd-pin-section",
+          start: "-15% top",
+          end: "200% top",
+          scrub: 1.5,
+          markers: true,
+          pin: true,
+        },
+      });
 
-    tl.to(".video-box", {
-      clipPath: "circle(100% at 50% 50%)",
-      ease: "power1.inOut",
-    });
+      tl.to(".video-box", {
+        clipPath: "circle(100% at 50% 50%)",
+        ease: "power1.inOut",
+      });
+    }
   });
   return (
     <section className="vd-pin-section">
       <div
         style={{
-          clipPath: "circle(0% at 50% 50%)",
+          clipPath: isMobile
+            ? "circle(100% at 50% 50%)"
+            : "circle(6% at 50% 50%)",
         }}
         className="size-full video-box"
       >
