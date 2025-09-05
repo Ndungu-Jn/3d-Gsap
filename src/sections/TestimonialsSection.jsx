6d6f25d353a0961bac09e7eb9225402da69b8c13
John Ndungu <ndungulink@gmail.com>
2025-09-05 10:08:51 +0300

    Add video playback functionality to TestimonialsSection

--- src/sections/TestimonialsSection.jsx
+++ src/sections/TestimonialsSection.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import { cards } from "../constants";
 
 const TestimonialsSection = () => {
+  const vdRef = useRef([]);
+  const handlePlay = (index) => {
+    const video = vdRef.current[index];
+    console.log(video);
+  };
+
   return (
     <section className="testimonials-section">
       <div className="absolute size-full flex flex-col items-center pt-[5vw]">
@@ -15,8 +21,11 @@ const TestimonialsSection = () => {
           <div
             key={index}
             className={`vd-card ${card.translation} ${card.rotation}`}
+            onMouseEnter={() => handlePlay(index)}
+            onMouseLeave={() => handlePause(index)}
           >
             <video
+              ref={(el) => (vdRef.current[index] = el)}
               src={card.src}
               playsInline
               muted
